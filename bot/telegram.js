579e3cba3df5d4b3493f8e9c8bcee9f62612ef8b
Creatorpoint <pk4030794@gmail.com>
2026-02-11 20:06:10 +0530

    Update telegram.js

--- bot/telegram.js
+++ bot/telegram.js
@@ -1,10 +1,12 @@
-const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.BOT_TOKEN,{polling:true});
-const axios = require("axios");
+import TelegramBot from "node-telegram-bot-api";
 
-bot.onText(/\/short (.+)/, async(msg,match)=>{
- const res = await axios.post("https://YOUR_DOMAIN/api/links/create",{
-   url: match[1], user: msg.from.username
- });
- bot.sendMessage(msg.chat.id, `Short Link:\nhttps://YOUR_DOMAIN/${res.data.short}`);
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+console.log("🤖 Telegram bot started");
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "🚀 Welcome to TrackifyX\nDashboard: " + process.env.BASE_URL
+  );
 });
