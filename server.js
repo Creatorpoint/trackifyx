d3c69e9407128067c6eefbeea2e6e3964ebf43ec
Creatorpoint <pk4030794@gmail.com>
2026-02-11 20:38:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
+import "dotenv/config";
+
+console.log("🌍 ENV loaded");
+
+import "./bot/bot.js";   // ⬅️ dotenv ke baad hi
+
 import express from "express";
-import redirectRoute from "./backend/routes/redirect.js";
-import campaignsRoute from "./backend/routes/campaigns.js";
-import db from "./backend/db.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-app.use("/r", redirectRoute);
-app.use("/api/campaigns", campaignsRoute);
-
-app.get("/", (req, res) => {
-  res.send("🚀 TrackifyX running!");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+import redirectRoute from "./routes/redirect.js";
+import campaignsRoute from "./routes/campaigns.js";
