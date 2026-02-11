db03c8c0891f4fc8f92b49d489af340e1617c643
Creatorpoint <pk4030794@gmail.com>
2026-02-11 19:11:31 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import redirectRoute from "./routes/redirect.js";
+import campaignsRoute from "./routes/campaigns.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/r", redirectRoute);
+app.use("/api/campaigns", campaignsRoute);
+
+app.get("/", (req, res) => {
+  res.send("🚀 TrackifyX is running");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
